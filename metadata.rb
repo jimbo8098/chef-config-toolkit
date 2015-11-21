6c4703693cd5f74a191101e2d99621b08e413b5e
Gavin Staniforth <gavin@usemarkup.com>
2015-11-21 06:41:59 +0000

    Added the depends

--- metadata.rb
+++ metadata.rb
@@ -3,3 +3,4 @@ license          'All rights reserved'
 description      'Chef Config Toolkit'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+depends           "hostsfile"
